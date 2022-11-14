c4ef44d415fcfd21e610ab772921b1c4d441cd7f
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-14 17:51:31 -0600

    r2 test pass

--- __tests__/dog.test.js
+++ __tests__/dog.test.js
@@ -2,24 +2,22 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const { Dog } = require('../lib/models/Dog');
 
 describe('dog routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
   test('post dog', async () => {
-    const dog = {
+    const dog = new Dog({
       name: 'lucy',
       breed: 'corgi',
       age: 2,
-    };
-    const res = await request(app).post('/dogs').send(dog);
-    expect(res.body.length).toEqual(7);
-    expect(res.body[0]).toEqual({
-      name: expect.any(String),
-      breed: expect.any(String),
-      age: expect.any(String),
     });
+    const res = await request(app).post('/dogs').send(dog);
+    expect(res.body.name).toEqual(dog.name);
+    expect(res.body.breed).toEqual(dog.breed);
+    expect(res.body.age).toEqual(dog.age);
   });
   afterAll(() => {
     pool.end();
