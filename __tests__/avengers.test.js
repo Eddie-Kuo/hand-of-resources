778c9956f87343508ba6398b8a4c7720baf79db9
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-15 12:10:59 -0600

    r3 post test pass

--- __tests__/avengers.test.js
+++ __tests__/avengers.test.js
@@ -2,6 +2,7 @@ const app = require('../lib/app');
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
+const { Avenger } = require('../lib/models/Avenger');
 
 describe('avengers routes', () => {
   beforeEach(() => {
@@ -9,15 +10,17 @@ describe('avengers routes', () => {
   });
 
   test('post route', async () => {
-    const avenger = {
+    const avenger = new Avenger({
       name: 'black widow',
       rating: 9,
       weakness: 'praying mantis',
-    };
+    });
     const res = await request(app).post('/avengers').send(avenger);
     expect(res.body.name).toEqual(avenger.name);
     expect(res.body.rating).toEqual(avenger.rating);
     expect(res.body.weakness).toEqual(avenger.weakness);
+    const count = await Avenger.count();
+    expect(count).toEqual(6);
   });
 
   afterAll(() => {
