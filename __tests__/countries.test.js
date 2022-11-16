9d4191986a16ac5de0293b1418c2cd0a70d5001b
Eddie-Kuo <Eddiekuo97@gmail.com>
2022-11-16 11:32:20 -0600

    r5 get all test

--- __tests__/countries.test.js
+++ __tests__/countries.test.js
@@ -7,6 +7,18 @@ describe('countries routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  test('/GET', async () => {
+    const res = await request(app).get('/api/v1/countries');
+    expect(res.length).toEqual(6);
+    expect(res.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      populationMillions: expect.any(Number),
+      location: expect.any(String),
+    });
+  });
+
   test('/POST', async () => {
     const country = {
       name: 'england',
